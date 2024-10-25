3c8896927ecaf892e849cd5804c2f2ffa2a4a6bc
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-10-25 15:25:50 +0200

    chore: change drizzle-kit version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@vercel/remix/vite";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -19,7 +18,6 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
       },
-      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
